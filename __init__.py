52ae1cd6f6bf681609f169daae5773d696bde306
Nils Moynac <e12016308@student.tuwien.ac.at>
2021-05-10 09:25:07 +0200

    create first intents

--- __init__.py
+++ __init__.py
@@ -1,4 +1,23 @@
 from adapt.intent import IntentBuilder
 from chatterbox import ChatterboxSkill, intent_handler
+from os import mkdir
+from os.path import join
+class FlashCardSkill(ChatterboxSkill):
+    def initialize(self):
+        data_dir = "data"
+        if not self.file_system.exists(data_dir):
+            mkdir(join(self.file_system.path, data_dir))
 
-# class FlashCardSkill(ChatterboxSkill):
+    @intent_handler('show.me.flashcards.intent')
+    def handle_do_you_like(self, message):
+        nbFlashcards = message.data.get('number')
+        if nbFlashcards is not None:
+            # self.speak_dialog('like.tomato.type', {'type': tomato_type})
+            self.log.info("number of wanted flashcards : " + str(nbFlashcards))
+        else:
+            self.log.info("number of wanted flashcards not specified")
+            # self.speak_dialog('like.tomato.generic')
+
+
+def create_skill():
+    return FlashCardSkill()
